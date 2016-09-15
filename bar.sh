69b14fcbc17203d341f0300a70f987cbe9a03480
GRBurst <JuliusE89@gmail.com>
2016-09-15 12:05:51 +0200

    Add local 'build script'

---
+++ bar.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+PACK=com.grburst.ttrorganizer
+ACT=.TTROrganizer
+APP=build/outputs/apk/ttrorganizer
+BUILD=packageDebug
+if (( $# ==  1 )); then
+    if [[ "$1" == "d" ]]; then
+        BUILD="packageDebug -a";
+        APP="$APP-debug-unaligned.apk"
+    elif [[ "$1" == "r" ]]; then
+        BUILD="assembleRelease";
+        APP="$APP-release.apk"
+    else
+        BUILD = $1
+        APP="$APP$2"
+    fi
+else
+    BUILD="$@";
+    APP="$APP-debug-unaligned.apk"
+fi
+
+
+echo "Building project $PACK with $BUILD"
+./gradlew $BUILD
+
+if [[ "$BUILD" == "packageDebug -a" ]]; then
+    echo "Installing $APP"
+    adb install -r $APP
+
+    echo "Starting Activity"
+    adb shell am start -n $PACK/$ACT
+fi
