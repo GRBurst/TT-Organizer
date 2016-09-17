50fc94ba4162fb99a12f3728ab3f5a23bdc445f4
GRBurst <JuliusE89@gmail.com>
2016-09-17 03:28:25 +0200

    TTRCalculator in beta version (somehow instable calculation -> check, bad conditions?)

--- src/main/scala/com/grburst/ttrorganizer/TTRCalculator.scala
+++ src/main/scala/com/grburst/ttrorganizer/TTRCalculator.scala
@@ -1,11 +1,13 @@
 package com.grburst.ttrorganizer
 
-import scala.math.{pow, round}
+import scala.math.{ pow, round }
+import scala.util.Try
 
 import org.scaloid.common._
 
 import android.app.Activity
 import android.graphics.Color
+import android.view._
 import android.view.Gravity
 import android.os.Bundle
 import android.widget._
@@ -16,10 +18,15 @@ import macroid.contrib.Layouts._
 import macroid.contrib.TextTweaks._
 import macroid.contrib.LpTweaks._
 import macroid.contrib.BgTweaks
+import macroid.Transformer
+import macroid.Transformer.Layout
 import macroid.FullDsl._
 import com.fortysevendeg.macroid.extras.LinearLayoutTweaks._
 
-class TTRCalculator() extends SActivity with Contexts[Activity] {
+// import scala.collection.mutable
+// import scala.collection.mutable.ListBuffer
+
+class TTRCalculator extends SActivity with Contexts[Activity] {
 
   val ak = 16
   val naf = 0
@@ -27,13 +34,35 @@ class TTRCalculator() extends SActivity with Contexts[Activity] {
   val ttrdiff = 0
   val oldTTR = 1800
 
-  def victories = 0
+  val victories = 1.0
 
-  def sum(a:Double) = a
+  // def sum(s:Double*) = s.reduce(_ + _)
 
-  def prob(playerA:Int, playerB:Int):Double = 1/(1 + Math.pow(10, (playerB.toDouble - playerA.toDouble)/150))
+  def prob(playerA: Double, playerB: Double): Double = 1 / (1 + Math.pow(10, (playerB - playerA) / 150))
 
-  def calcNewTTR() = oldTTR + Math.round(victories - sum(prob(1800, 1700)) * ak)
+  // def calcNewTTR() = oldTTR + Math.round(victories - (oppValues.map(o => prob(oldTTR, (o.get.getText).toString.toDouble))).sum * ak)
+
+  def calcNewTTR(oppValues: List[Double]) = oldTTR + Math.round(victories - (oppValues.map(o => prob(oldTTR, o))).sum * ak)
+
+  def getTTR(mView: Option[macroid.contrib.Layouts.VerticalLinearLayout]): String = {
+
+    def getValues(view: ViewGroup): List[Double] = {
+      ((0 until view.getChildCount()) flatMap { i =>
+        view.getChildAt(i) match {
+          case v: ViewGroup => getValues(v)
+          case v: EditText => Try(v.getText.toString.toDouble).toOption
+          case _ => Nil
+        }
+      }).toList
+    }
+
+    calcNewTTR(getValues(mView.get)).toString
+  }
+
+  def addOpponent = {
+    val oppField = l[HorizontalLinearLayout](w[TextView] <~ text("Gegner: ") <~ wrapContent, w[EditText] <~ numeric <~ matchWidth <~ fadeIn(500))
+    addViews(Seq(oppField))
+  }
 
   override def onCreate(savedInstanceState: Bundle) = {
     super.onCreate(savedInstanceState)
@@ -41,27 +70,20 @@ class TTRCalculator() extends SActivity with Contexts[Activity] {
     var oppFields = slot[VerticalLinearLayout]
     var wRes = slot[TextView]
 
-    val oppField = l[HorizontalLinearLayout](w[TextView] <~ text("Gegner: ") <~ wrapContent, w[EditText] <~ numeric <~ matchWidth <~ fadeIn(500))
-
     setContentView {
       getUi {
         l[VerticalLinearLayout](
           w[TextView] <~ text("TTR Rechner"),
-          l[VerticalLinearLayout](oppField) <~ wire(oppFields),
+          l[VerticalLinearLayout](
+            l[HorizontalLinearLayout](w[TextView] <~ text("Eigener TTR: ") <~ wrapContent, w[EditText] <~ numeric <~ matchWidth <~ fadeIn(500))) <~ wire(oppFields),
           l[HorizontalLinearLayout](
-            w[Button] <~ text("Berechne") <~ On.click(wRes <~ text(calcNewTTR().toString)),
-            w[Button] <~ text("+") <~ On.click(oppFields <~ addViews(Seq(oppField)))
-          ),
+            w[Button] <~ text("Berechne") <~ On.click(wRes <~ text(getTTR(oppFields))),
+            w[Button] <~ text("+") <~ On.click(oppFields <~ addOpponent)),
           l[VerticalLinearLayout](
             w[TextView] <~ text("Neuer TTR") <~ size(25) <~ matchWidth <~ Tweak[TextView](_.setGravity(Gravity.CENTER_HORIZONTAL)),
-            w[TextView] <~ wire(wRes) <~ size(50) <~ matchWidth <~ Tweak[TextView](_.setGravity(Gravity.CENTER_HORIZONTAL))
-          )
-        ) <~ padding(left = 4 dp, right = 4 dp)
+            w[TextView] <~ wire(wRes) <~ size(50) <~ matchWidth <~ Tweak[TextView](_.setGravity(Gravity.CENTER_HORIZONTAL)))) <~ padding(left = 4 dp, right = 4 dp)
       }
     }
   }
 
 }
-  // (1/(1+Math.pow(10,(parseFloat(document.calc.gegnera.value) - parseFloat(document.calc.dttr.value))/150)))*100)
-  // document.calc.ttrneu.value = naf + parseFloat(document.calc.dttr.value) + Math.round((siege - parseFloat(document.calc.output1.value)/100 - parseFloat(document.calc.output2.value)/100 - parseFloat(document.calc.output3.value)/100 - parseFloat(document.calc.output4.value)/100 - parseFloat(document.calc.output5.value)/100 - parseFloat(document.calc.output6.value)/100 - parseFloat(document.calc.output7.value)/100 - parseFloat(document.calc.output8.value)/100 - parseFloat(document.calc.output9.value)/100 - parseFloat(document.calc.output10.value)/100)*ak)
-  // document.calc.ttrdiff.value = document.calc.ttrneu.value - parseFloat(document.calc.dttr.value)
