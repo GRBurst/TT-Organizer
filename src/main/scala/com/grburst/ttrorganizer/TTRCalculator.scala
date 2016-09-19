cdebcf97fa267788db324e391b732d42383f8337
GRBurst <JuliusE89@gmail.com>
2016-09-19 13:56:54 +0200

    Redesign calculator to ignore lines without ttr value

--- src/main/scala/com/grburst/ttrorganizer/TTRCalculator.scala
+++ src/main/scala/com/grburst/ttrorganizer/TTRCalculator.scala
@@ -1,9 +1,12 @@
 package com.grburst.ttrorganizer
 
+import com.grburst.ttrorganizer.androidHelper._
+
 import scala.math.{ pow, round }
 import scala.util.Try
 
 import org.scaloid.common._
+import scala.collection.mutable
 
 import android.app.Activity
 import android.graphics.Color
@@ -24,67 +27,50 @@ import macroid.FullDsl._
 
 import com.fortysevendeg.macroid.extras.LinearLayoutTweaks._
 
-case class SamplePlayer(ttr:Int,
-  vRang:Int,
-  dRang:Int,
-  name:String,
-  verein:String,
-  ak:Int = 16)
+//TODO:
+//  - Add advanced options to calculation
+//  - Implement TextWatcher to automatically update new ttr with 4 numbers are entered
+//  - Automatically recalculate if checkbox is changed
+//  - Listview for opponents -> currently if #opponenets > 5 new ttr not viewable anymore
 
 class TTRCalculator extends SActivity with Styles with Contexts[Activity] {
 
-  val player = SamplePlayer(1800, 1, 1, "Blub", "TTV Test", 16)
+  case class CalcField(eSlot: Option[android.widget.EditText], cSlot: Option[android.widget.CheckBox]) {
+
+    var ttrValue = eSlot
+    var victoryBox = cSlot
+
+    def getTTRValue: Option[Double] = Try(ttrValue.get.text.toString.toDouble).toOption
+    def hasValidTTR: Boolean = getTTRValue.isDefined
+
+    def isVictorious: Option[Boolean] = Try(victoryBox.get.isChecked).toOption
+  }
+
   // age < 16 => ak + 8
   // age < 21 => ak + 4
   // < 30 graded matches => ak + 4
   // no graded match in 365 days => +4 for 15 matches
   // Nachwuchs-Ausgleich = 0 => newTTR + 2
 
-  //TODO:
-  //  - Add advanced options to calculation
-  //  - Implement TextWatcher to automatically update new ttr with 4 numbers are entered
-  //  - Only add checkbox if number field is not empty
-  //  - Automatically recalculate if checkbox is changed
-  //  - Listview for opponents -> currently if #opponenets > 5 new ttr not viewable anymore
-
   def prob(playerA: Double, playerB: Double): Double = 1.0 / (1.0 + Math.pow(10, (playerB - playerA) / 150.0))
 
-  def calcNewTTR(ownTTR:Double, oppTTR: List[Double], victories:Int, ak:Int, na:Int):Int = (ownTTR + Math.round((victories - (oppTTR.map(o => prob(ownTTR, o))).sum) * ak) + na).toInt
+  def calcNewTTR(ownTTR: Double, oppTTR: List[Double], victories: Int, ak: Int, na: Int): Int = (ownTTR + Math.round((victories - (oppTTR.map(o => prob(ownTTR, o))).sum) * ak) + na).toInt
 
-  def getTTR(mView: Option[macroid.contrib.Layouts.VerticalLinearLayout]): String = {
-
-    def getTTRValues(view: ViewGroup): List[Double] = {
-      ((0 until view.getChildCount()) flatMap { i =>
-        view.getChildAt(i) match {
-          case v: ViewGroup => getTTRValues(v)
-          case v: EditText => Try(v.getText.toString.toDouble).toOption
-          case _ => Nil
-        }
-      }).toList
-    }
-
-    def getVictories(view: ViewGroup): List[Int] = {
-      ((0 until view.getChildCount()) flatMap { i =>
-        view.getChildAt(i) match {
-          case v: ViewGroup => getVictories(v)
-          case v: CheckBox  => if(v.isChecked) List(1) else Nil
-          case _ => Nil
-        }
-      }).toList
-    }
-
-    val ttrVals = getTTRValues(mView.get)
-    if(!ttrVals.drop(1).isEmpty) calcNewTTR(ttrVals.head, ttrVals.drop(1), getVictories(mView.get).sum, 16, 0).toString
-    else player.ttr.toString
+  def getTTR(fields: mutable.ArrayBuffer[CalcField]): String = {
+    val victories: Int = fields.drop(1).count(l => l.hasValidTTR && l.isVictorious.getOrElse(false))
+    val ttrVals: List[Double] = fields.drop(1).map(l => l.getTTRValue).flatten.toList
+    if (!ttrVals.isEmpty && fields.head.hasValidTTR) calcNewTTR(fields.head.getTTRValue.get, ttrVals, victories, 16, 0).toString
+    else if (fields.head.hasValidTTR) fields.head.getTTRValue.get.toInt.toString
+    else ""
   }
 
-  def addOpponent = {
-    val oppField = l[HorizontalLinearLayout](
+  def addOpponent(fields: mutable.ArrayBuffer[CalcField]) = {
+
+    fields += CalcField(slot[EditText], slot[CheckBox])
+    addViews(Seq(l[HorizontalLinearLayout](
       w[TextView] <~ text("Gegner: ") <~ l_weight(0.3f),
-      w[EditText] <~ numeric <~ l_weight(0.3f),
-      w[CheckBox] <~ l_weight(0.3f)
-    ) <~ fadeIn(500)
-    addViews(Seq(oppField))
+      w[EditText] <~ numeric <~ l_weight(0.3f) <~ wire(fields.last.ttrValue),
+      w[CheckBox] <~ l_weight(0.3f) <~ wire(fields.last.victoryBox)) <~ fadeIn(500)))
   }
 
   override def onCreate(savedInstanceState: Bundle) = {
@@ -94,32 +80,30 @@ class TTRCalculator extends SActivity with Styles with Contexts[Activity] {
     var advOpt = slot[VerticalLinearLayout]
     var wRes = slot[TextView]
 
+    val calcFields: mutable.ArrayBuffer[CalcField] = mutable.ArrayBuffer()
+
+    calcFields += CalcField(slot[EditText], None)
+
     setContentView {
-        (l[VerticalLinearLayout](
-          w[TextView] <~ text("TTR Rechner"),
-          l[VerticalLinearLayout](
-            l[HorizontalLinearLayout](
-              w[TextView] <~ text("Eigener TTR: ") <~ l_weight(0.3f),
-              w[EditText] <~ numeric <~ text(player.ttr.toString) <~ l_weight(0.3f),
-              w[TextView] <~ text("Sieg?") <~ l_weight(0.3f)
-            )
-          ) <~ wire(oppFields),
-          l[VerticalLinearLayout](
-            w[CheckBox] <~ text("Unter 16 Jahre"),
-            w[CheckBox] <~ text("Unter 21 Jahre"),
-            w[CheckBox] <~ text("Weniger als 30 gewertete Spiele"),
-            w[CheckBox] <~ text("Seid 365 Tagen kein gewertetes Spiel"),
-            w[CheckBox] <~ text("Nachwuchs-Ausgleich")
-          ) <~ wire(advOpt) <~ hide,
+      (l[VerticalLinearLayout](
+        w[TextView] <~ text("TTR Rechner"),
+        l[VerticalLinearLayout](
           l[HorizontalLinearLayout](
-            w[Button] <~ text("Berechne!") <~ On.click(wRes <~ text(getTTR(oppFields))),
-            w[Button] <~ text("Weitere Optionen") <~ On.click(advOpt <~ show),
-            w[Button] <~ text("+ Gegner") <~ On.click(oppFields <~ addOpponent)
-          ),
-          w[TextView] <~ text("Neuer TTR") <~ size(25) <~ matchWidth <~ Tweak[TextView](_.setGravity(Gravity.CENTER_HORIZONTAL)),
-          w[TextView] <~ wire(wRes) <~ size(50) <~ matchWidth <~ Tweak[TextView](_.setGravity(Gravity.CENTER_HORIZONTAL))
-        ) <~ padding(left = 4 dp, right = 4 dp)
-      ).get
+            w[TextView] <~ text("Eigener TTR: ") <~ l_weight(0.3f),
+            w[EditText] <~ numeric <~ text("1800") <~ l_weight(0.3f) <~ wire(calcFields.head.ttrValue),
+            w[TextView] <~ text("Sieg?") <~ l_weight(0.3f))) <~ wire(oppFields),
+        l[VerticalLinearLayout](
+          w[CheckBox] <~ text("Unter 16 Jahre"),
+          w[CheckBox] <~ text("Unter 21 Jahre"),
+          w[CheckBox] <~ text("Weniger als 30 gewertete Spiele"),
+          w[CheckBox] <~ text("Seid 365 Tagen kein gewertetes Spiel"),
+          w[CheckBox] <~ text("Nachwuchs-Ausgleich")) <~ wire(advOpt) <~ hide,
+        l[HorizontalLinearLayout](
+          w[Button] <~ text("Berechne!") <~ On.click(wRes <~ text(getTTR(calcFields))),
+          w[Button] <~ text("Weitere Optionen") <~ On.click(advOpt <~ show),
+          w[Button] <~ text("+ Gegner") <~ On.click(oppFields <~ addOpponent(calcFields))),
+        w[TextView] <~ text("Neuer TTR") <~ size(25) <~ matchWidth <~ Tweak[TextView](_.setGravity(Gravity.CENTER_HORIZONTAL)),
+        w[TextView] <~ wire(wRes) <~ size(50) <~ matchWidth <~ Tweak[TextView](_.setGravity(Gravity.CENTER_HORIZONTAL))) <~ padding(left = 4 dp, right = 4 dp)).get
     }
   }
 
